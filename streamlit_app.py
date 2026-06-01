7572a7c2c692b8f4430b99e0e8c8bb934a414024
DamjanPopovski02 <damjan.popovski@in516ht.com>
2026-06-01 13:40:55 +0200

    Update session handling for Snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ streamlit.write(
 name_on_order = streamlit.text_input ('Name of Smoothie: ')
 streamlit.write('The name on your Smoothie will be: ', name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
